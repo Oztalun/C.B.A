e62b48e8fb0e863659d3773fc6a8da5d745485c9
Kyuho09 <noaetoile1@gmail.com>
2024-07-04 20:24:25 +0900

    반복문 변경, 주석 추가

--- class/kh_class.py
+++ class/kh_class.py
@@ -11,15 +11,29 @@ class Member:
         print(f"회원 아이디 : {self.username}")
         print(f"회원 비밀번호 : {self.hash_password}")
 
+# members 리스트 생성
 members = []
-member_cnt = int(input("몇 명의 정보를 입력하시겠습니까? : "))
-for i in range(member_cnt):
-    print(f"{i+1}번째 회원 정보를 입력해주세요.")
+
+while True:
+    print("회원 정보를 입력해주세요.")
     name = input("이름 : ")
     username = input("아이디 : ")
     password = input("비밀번호 : ")
     members.append(Member(name, username, password))
     print("-------------------------------")
 
+    # 한번 정보 입력할 때마다 추가 입력 질문을 출력, 예외처리까지 설정
+    while True:
+        retry = input("추가로 정보를 입력하시겠습니까? (y/n) : ").lower()
+        if retry == 'y' or retry == 'n':
+            break
+        else:
+            print("(y/n)을 입력하세요.")
+
+    if retry == 'n':
+        break
+
+# 회원 정보(이름) 출력
 for i in members:
-    i.display()
+    # i.display()
+    print(i.name)
